233042ac50403a94f02fa6eeb3060e733fc1eeff
Alok <alokkyyadav@gmail.com>
2023-06-08 19:36:47 +0530

    "polymorphism assignment"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[]args) {
+        RWOnly obj = new RWOnly();
+
+//        obj.name = "ALOK";
+//        System.out.println(obj.name);
+//        java: name has private access in com.driver.RWOnly
+
+        obj.setName("ALOK");
+        System.out.println(obj.getName());
+    }
   
 }
